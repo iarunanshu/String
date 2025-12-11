621fad622b726ad26023eb1acdd3c21fc706c4db
Arunanshu jena <102849287+iarunanshu@users.noreply.github.com>
2025-12-11 23:59:14 +0530

    Update Anagram.java

--- Anagram.java
+++ Anagram.java
@@ -2,6 +2,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Anagram {
+//hi
     static final int CHAR=256;
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
